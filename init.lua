3efbd2e41c232660be6a7c5f9e199eb687f1a9b9
Anthony Halim <anthony.halim@live.com>
2023-06-03 00:50:06 +0800

    Add explorer tree

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require("user.telescope")
 require("user.treesitter")
 require("user.lsp")
 require("user.themes")
+require("user.explorertree")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/user/set.lua
+++ lua/user/set.lua
@@ -1,3 +1,10 @@
+-- nvim-tree: disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- nvim-tree: set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 -- Set highlight on search
 vim.o.hlsearch = false
 vim.o.incsearch = true
