61c2e5839f0c09364ba6f6c27dcfa42c96f65ba2
Anthony Halim <anthony.halim@live.com>
2023-06-29 10:45:51 +0800

    Switch to lazy starter

--- init.lua
+++ init.lua
@@ -1,16 +1,2 @@
-require("user.set")           -- Must be first to ensure correct settings 
-require("user.packages")      -- Must be before any other packages are used
-
-require("plugins.lualine")
-require("plugins.telescope")
-require("plugins.treesitter")
-require("plugins.lsp")
-require("plugins.nvim-tree")
-require("plugins.comment")
-require("plugins.bufferline")
-require("plugins.themes")
-
-require("user.remap")         -- Must be after plugins to ensure overriding
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
